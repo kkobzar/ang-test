13d4dcc764192dd681983c25bc0a5e4a300734cc
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-04-30 16:49:33 +0300

    random avatars

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Contact} from "../../Contact";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
@@ -8,11 +9,23 @@ import {Contact} from "../../Contact";
 })
 export class ContactComponent implements OnInit{
   @Input() contact: Contact
+  avatar:string = ''
 
-  constructor() {
+  constructor(private http:HttpClient) {
   }
 
   ngOnInit() {
+    this.http.get<any>('https://randomuser.me/api/?seed='+this.contact.id)
+      .subscribe({
+        next: data => {
+          if(typeof data.results !== 'undefined' &&
+          typeof data.results[0].picture.large !== 'undefined')
+            this.avatar = data.results[0].picture.large
+        },
+        error: () =>{
+          this.avatar = ''
+        }
+      })
   }
 
 }
